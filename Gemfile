65d4a46e24562ad4ab2b509c3b55ae8d5d2e98a4
Nikos Segkos <nksegos@pm.me>
2025-02-05 23:30:54 +0200

    f this

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem "jbuilder"
 
 gem "devise"
 
+gem "bootstrap_form"
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
