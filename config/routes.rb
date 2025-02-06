8798e07b22183dde46f5745c02cdc040dfc96544
Nikos Segkos <nksegos@pm.me>
2025-02-06 21:02:18 +0200

    update looks once again and setup user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
   root 'pages#index'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_05_191918) do
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
